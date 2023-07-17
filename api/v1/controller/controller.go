7bda201ff41ff3b493a188e2a07da22c557c6f47
molly <email@example.com>
2023-07-17 12:17:17 +0800

    增加了注册路由，更新路由差鉴权中间件，没有错误处理

--- api/v1/controller/controller.go
+++ api/v1/controller/controller.go
@@ -1,3 +1,28 @@
 package controller
 
 //从路由请求中解析参数和结构体，调用 Model 和 Service，处理业务逻辑，决定如何响应用户的请求，处理异常和错误
+
+import (
+	"team_todo/model"
+	"team_todo/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Register(c *gin.Context){
+	var userReq model.User
+	userReq.Email = c.Postform("email")
+	userReq.Password = c.Postform("password")
+	userReq.Nickname = c.Postform("nickname")
+
+	service.Register(userReq)
+	
+}
+
+func Update(c *gin.Context){
+	var userReq model.User
+	userReq.Nickname = c.Postform("nickname")
+	userReq.Avatar = c.Postform("avatar")
+
+	service.Modify(userReq)
+}
--- api/v1/route.go
+++ api/v1/route.go
@@ -1,4 +1,19 @@
 package route
 
+import (
+	"team_todo/api/v1/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
 //设置路由
 //登录、注册、找回密码...见api文档
+
+//注册和更新
+func MyRouter (r *gin.Engine){
+	//注册
+r.POST("/user/register",controller.Register)
+	//更新
+r.PUT("/user/profile",//鉴权中间件,controller.Update
+)
+}
