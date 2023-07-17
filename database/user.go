1a2775d65d9d6a48286fb1da5768cdd25b4c7fe5
molly <email@example.com>
2023-07-17 11:40:47 +0800

    补充了密码加密

--- database/user.go
+++ database/user.go
@@ -4,13 +4,26 @@ import (
 	"fmt"
 	"team_todo/model"
 	"team_todo/global"
-
+	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	"crypto/sha256"
+	"encoding/hex"
+
 )
+
+//加密
+func Myencrypt(row string) string{
+    data := []byte(row)
+    hash := sha256.Sum256(data)
+    fmt.Printf("%x\n", hash)
+	res := hex.EncodeToString(hash[:])
+	return res
+}
 
 // 由service调用的注册函数
 func Register(userinfo model.User) {
 	//DB是数据库连接
+
 	exist := Exists(global.GVA_DB, "users", "email", userinfo.Email)
 
 	// fmt.Println("whether the email is used: ", exist)
@@ -22,6 +35,7 @@ func Register(userinfo model.User) {
 		fmt.Println("The email is already registered.Change a email number or log in directly.")
 		return
 	} else {
+		userinfo.Password = (userinfo.Password)
 		if userinfo.Password != "" && userinfo.Nickname != "" {
 			global.GVA_DB.Create(&user)
 			fmt.Println("oh created")
@@ -48,15 +62,9 @@ func Login(userinfo model.User) {
 		var being_logged model.User
 		global.GVA_DB.Table("users").Where("email = ?", userinfo.Email).First(&being_logged)
 
-		//err3 := db.Table("users").Where("phone_number = ?", user.PhoneNumber).First(&being_logged).Error
-
-		//checkerr(err3)
-
-		// fmt.Println("user password: ", user.Password, "user Nickname: ", user.NickName, "right password: ", being_logged.Password, "right nickname: ", being_logged.NickName)
-		// fmt.Println("the user who is being logged : ", being_logged.PhoneNumber, being_logged.Password)
-		// fmt.Println("user password: ", user.Password, "right password: ", being_logged.Password)
+		
 		switch {
-		case userinfo.Password == being_logged.Password:
+		case Myencrypt(userinfo.Password) == being_logged.Password:
 			fmt.Println("log in successfully")
 			//登录成功标识......
 			//......
@@ -76,3 +84,33 @@ func Modify(userinfo model.User,nickname string,avatar string) {
 	userReq.Avatar = avatar
 	global.GVA_DB.Model(&model.User{}).Updates(userReq)
 }
+/*
+//上传头像图片函数
+func UploadAvatar(c *gin.Context) {
+	// 从请求中获取上传的文件
+	file, err := c.FormFile("avatar")
+	if err != nil {
+		// 处理错误
+		c.JSON(400, gin.H{
+			"error": "Failed to retrieve avatar file",
+		})
+		return
+	}
+
+	// 将文件保存到服务器端指定的目录或云存储服务中
+	err = c.SaveUploadedFile(file, "/path/to/avatar_directory/"+file.Filename)
+	if err != nil {
+		// 处理错误
+		c.JSON(500, gin.H{
+			"error": "Failed to save avatar file",
+		})
+		return
+	}
+
+	// 返回上传后的图片链接
+	avatarURL := "https://example.com/avatars/" + file.Filename
+	c.JSON(200, gin.H{
+		"url": avatarURL,
+	})
+}
+*/
