95c5aa3ee2b45e8e7314ba92c8f3bcda84aab19c
648lsp <138429322+648lsp666@users.noreply.github.com>
2023-07-21 01:43:49 +0800

    修改获取用户信息功能

--- templates/javascipts/getprofile.js
+++ templates/javascipts/getprofile.js
@@ -1,17 +1,33 @@
+axios.defaults.headers.common['Authorization'] = `Bearer ${jwtToken}`;
 
-function ondomcontentloaded(){
+async function ondomcontentloaded(){
     let useravatar =  document.getElementById('avatar');
-    let userid = document.getElementById('usersID');
-    axios.get("/users/${userID}/profile")
-    .then(response =>{
-        useravatar.src = response.avatar;
-        userid.innerHTML = response.id; 
-    })
-    .catch(_error =>{
+    let usernickname = document.getElementById('usersID');
+    let changepage_avatar = document.getElementById('changepage_useravatar')
+    let changepage_name = document.getElementById('changepage_username');
+    try {
+        const userprofile = await axios.get(`/users/${usernickname}/profile`);
+        const userprofile_response = userprofile.data;
+        if(userprofile_response){
+            changepage_avatar.src = userprofile_response.avatar;
+            changepage_name.innerHTML = userprofile_response.nickname;
+            useravatar.src = userprofile_response.avatar;
+            usernickname.innerHTML = userprofile_response.nickname;
+        }else{
+
+            useravatar.src = '../img/user-regular.svg';
+            usernickname.innerHTML = '';
+            changepage_avatar.src = '../img/user-regular.svg';
+            changepage_name.innerHTML = '';
+        }
+            
+    }catch(error){
         useravatar.src = '../img/user-regular.svg';
-        userid.innerHTML = '';
-    })
+        usernickname.innerHTML = '';
+        changepage_avatar.src = '../img/user-regular.svg';
+        changepage_name.innerHTML = '默认idxxx';
+    }
 }
 
-document.addEventListener('DOMContentLoaded',ondomcontentloaded);
+document.addEventListener('DOMContentLoaded', ondomcontentloaded);
 
