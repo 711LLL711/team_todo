a33bf4c94b4ac830400b943cbe009ae5835e48fb
648lsp <138429322+648lsp666@users.noreply.github.com>
2023-07-21 17:12:39 +0800

    Update getprofile.js

--- static/javascipts/getprofile.js
+++ static/javascipts/getprofile.js
@@ -6,25 +6,24 @@ async function ondomcontentloaded(){
     let changepage_avatar = document.getElementById('changepage_useravatar')
     let changepage_name = document.getElementById('changepage_username');
     try {
-        const userprofile = await axios.get(`/users/${usernickname}/profile`);
-        const userprofile_response = userprofile.data;
-        if(userprofile_response){
-            changepage_avatar.src = userprofile_response.avatar;
-            changepage_name.innerHTML = userprofile_response.nickname;
-            useravatar.src = userprofile_response.avatar;
-            usernickname.innerHTML = userprofile_response.nickname;
+        const userprofile = await axios.get(`/users/?${localid}/profile`);
+        if(!userprofile.error){
+            changepage_avatar.src = userprofile.avatar;
+            changepage_name.innerHTML = userprofile.nickname;
+            useravatar.src = userprofile.avatar;
+            usernickname.innerHTML = userprofile.nickname;
         }else{
 
-            useravatar.src = '../img/user-regular.svg';
+            useravatar.src = '../images/user-regular.svg';
             usernickname.innerHTML = '';
-            changepage_avatar.src = '../img/user-regular.svg';
+            changepage_avatar.src = '../images/user-regular.svg';
             changepage_name.innerHTML = '';
         }
             
     }catch(error){
-        useravatar.src = '../img/user-regular.svg';
+        useravatar.src = '../images/user-regular.svg';
         usernickname.innerHTML = '';
-        changepage_avatar.src = '../img/user-regular.svg';
+        changepage_avatar.src = '../images/user-regular.svg';
         changepage_name.innerHTML = '默认idxxx';
     }
 }
